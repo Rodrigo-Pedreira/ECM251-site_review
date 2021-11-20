5d066299ba408545c2fa2e04feb077b81b8a3517
Rodrigo-Pedreira <79457805+Rodrigo-Pedreira@users.noreply.github.com>
2021-11-20 04:06:57 -0300

    Versao que pega e exibe uma lista dos filmes.
    - FutureBuilder;
    - Arquivo para os enderecos web;
    - Removido os enderecos web do network helper;

--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,8 @@
-/* Default
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // home: const ListaFilmes(),
-    );
-  }
-}
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}*/
-
-import 'package:flutter/material.dart';
-
-import 'film.dart';
 import 'network_helper.dart';
+import 'film.dart';
+import 'our_web_adresses.dart';
 
 void main() => runApp(const MyApp());
 
@@ -141,20 +12,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Welcome to Flutter',
-      home: FilmListScroll(),
+      title: 'Lista de filmes', // Aparece como o nome da tab no navegador.
+      home: FilmList(), // Pagina web, Lista dos Filmes.
     );
   }
 }
 
-class FilmListScroll extends StatefulWidget {
-  const FilmListScroll({Key? key}) : super(key: key);
+/// Exibe um *titulo* e uma **lista com todos os Filmes**, alem de buscar os filmes (**http get**).
+class FilmList extends StatefulWidget {
+  const FilmList({Key? key}) : super(key: key);
 
   @override
-  _FilmListScrollState createState() => _FilmListScrollState();
+  _FilmListState createState() => _FilmListState();
 }
 
-class _FilmListScrollState extends State<FilmListScroll> {
+class _FilmListState extends State<FilmList> {
+  /// A *String* nao formatada(json) dos objetos filmes obtidos(http get).
+  Future? _filmRequestBody;
+
+  /// A **Lista** de todos os **filmes** obtidos.
+  final _filmlist = <Film>[];
+
+  /* Lista de filmes para testes.
+  
   final _filmlist = <Film>[
     Film(
         id: 1,
@@ -168,23 +48,75 @@ class _FilmListScrollState extends State<FilmListScroll> {
         genre: "genre",
         director: "director2",
         date: "date")
-  ];
-  // final _listfontstyle = const TextStyle(fontSize: 15);
+  ]; */
+
+  /// Executa so uma vez, quando a pagina carrega. Busca e cria a Lista com todos os filmes.
+  @override
+  void initState() {
+    super.initState();
+
+    // Fazendo HTTP GET, dando parse na resposta e prenchendo a lista de filmes.
+    _filmRequestBody =
+        Networkhelper.getDataBody(OurWebAdresses.filmRepositoryURL);
+
+    // Prenche a lista de filmes (apos esperar resposta http).
+    _filmRequestBody
+        ?.then((value) => _filmlist.addAll(Film.listFromJson(value)));
+    /*  .catchError((error) => handleError(error)); 
+       Pode tratar erros (agora esta erros sao tratados por um if em _buildFutureList). */
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Lista Filmes"),
+        title: const Text("Lista Filmes"), // Titulo da pagina.
       ),
-      body: _buildFilmList(),
+      body:
+          _buildFutureList(), // Corpo da pagina, exibe a Lista de todos os Filmes.
+
+      /* Um botao, ainda pode ser usado para testes se mudar o body tambem.
+
       floatingActionButton: FloatingActionButton(
         onPressed: _requestFilmList,
         tooltip: 'Load Film List',
         child: const Icon(Icons.add),
-      ),
+      ), */
     );
   }
+
+  /// Exibe a Lista de todos os Filmes ou erro ou icone de carregando.
+  Widget _buildFutureList() {
+    return FutureBuilder(
+      future: _filmRequestBody,
+      builder: (ctx, snapshot) {
+        if (snapshot.hasData) {
+          return _buildFilmList(); // Controi a Lista dos filmes.
+        } else if (snapshot.hasError) {
+          return const Text("HTTP GET Failed"); // Indica erro.
+        } else {
+          // Mostra um Icone de carregamento e Texto no centro (display antes do future acabar).
+          return Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const <Widget>[
+                SizedBox(
+                  child: CircularProgressIndicator(),
+                  width: 60,
+                  height: 60,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting results...'),
+                )
+              ]));
+        }
+      },
+    );
+  }
+
+/* Funcao teste antiga usada em conjunto com o floating button para adicionar os filmes na lista.
 
   void _requestFilmList() {
     Future filmRequest = Networkhelper.getData(Networkhelper.filmRepositoryURL);
@@ -195,15 +127,17 @@ class _FilmListScrollState extends State<FilmListScroll> {
       });
     });
   }
+*/
 
+  /// Constroi a Lista dos filmes (nome e diretor).
   Widget _buildFilmList() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
-        itemCount: _filmlist.length,
+        itemCount: _filmlist.length, // Tamanho da lista.
         itemBuilder: (BuildContext _contex, int i) {
           return ListTile(
-            title: Text(_filmlist[i].name),
-            subtitle: Text(_filmlist[i].director),
+            title: Text(_filmlist[i].name), // Nome do filme.
+            subtitle: Text(_filmlist[i].director), //  Diretor do filme.
           );
         });
   }
--- lib/network_helper.dart
+++ lib/network_helper.dart
@@ -1,12 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class Networkhelper {
-  static const webSiteURL = "http://localhost:8080";
-  static const filmRepositoryURL   = "$webSiteURL/film/selectAll";
-  static const userRepositoryURL   = "$webSiteURL/user/selectAll";
-  static const reviewRepositoryURL = "$webSiteURL/review/selectAll";
-
-  static Future getData(url) async {
+  static Future getDataBody(url) async {
     http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       // print(response.statusCode);
